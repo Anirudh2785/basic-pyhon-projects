1f7c9e78d43056a705f936ac2f2395f04cc068e2
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-06 17:13:16 +0530

    grade

---
+++ grade.py
@@ -0,0 +1,19 @@
+sub1 = int(input("enter your 1'st subujet mark :"))
+sub2 = int(input("enter your 2'nd subujet mark :"))
+sub3 = int(input("enter your 3'rd subujet mark :"))
+sub4 = int(input("enter your 4'th subujet mark :"))
+sub5 = int(input("enter your 5'th subujet mark :"))
+
+add =   sub1 + sub2 + sub3 + sub4 + sub5
+pct = add / 500 * 100 
+
+if pct > 90:
+    print(" grade : A")
+elif pct > 80 and pct <= 90:
+    print(" grade : B")
+elif pct > 70 and pct <= 80:
+    print(" grade : c")
+elif pct > 33 and pct <= 70:
+    print(" grade : D")
+else :
+    print(" grade : F ")
