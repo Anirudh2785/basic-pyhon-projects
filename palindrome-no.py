605bb758cc4b3b10ed4d2b7d91a48b415619f54f
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-09 00:05:45 +0530

    Create palindrome-no.py

---
+++ palindrome-no.py
@@ -0,0 +1,13 @@
+no = input("enter number :")
+no = (list(map(int,no))) 
+
+a = (no[::-1])
+b = (no[1::])
+
+if a == b :
+    print("true")
+elif a != b :
+    print("false")
+else :
+    print("error")
+
