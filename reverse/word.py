6dc18d8eba21c84d818c59efbe98897998216554
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-09 20:59:28 +0530

    Update word.py

--- reverse/word.py
+++ reverse/word.py
@@ -1,2 +1,13 @@
-word = input("enter word : ")
-print(word[::-1])
+# word = input("enter word : ")
+# print(word[::-1])
+
+w = input("enter word : ")
+
+wo = ""
+i = len(w) - 1
+
+while i >= 0 :
+    wo = wo + w[i]
+    i -= 1
+
+print("your revers word is ",wo)
