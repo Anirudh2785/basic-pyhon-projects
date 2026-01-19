9ea902a4010282bb27224d0442c1b12bf8dd5dc7
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-19 15:02:59 +0530

    Create gcd.py

---
+++ GCDorHCD/gcd.py
@@ -0,0 +1,35 @@
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+
+n1 = int(num1*0.5)
+n2 = int(num2*0.5)
+
+up = []
+down = []
+
+for i in range (1,n1+1) :
+    rem1 = num1%i
+    if rem1 == 0 :
+        div1 = int(num1/i)
+        if div1 == i :
+            up.append(i)
+        else :
+            up.append(i)
+            up.append(div1)
+            for o in range (1,n2+1) :
+                rem2= num2%o
+                if rem2 == 0 :
+                    div2 = int(num2/o)
+                    if div2 == o :
+                        down.append(o)
+                    else :
+                        down.append(o)
+                        down.append(div2)
+    else :
+        print(end="")
+
+up.sort()
+down.sort()
+
+if up == down :
+    print(up , end=" ")
